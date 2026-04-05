c62a2e76817ed0733b35289289a0eab545cde447
Mt08-Arch <273193637+Mt08-Arch@users.noreply.github.com>
2026-04-05 13:38:02 +0000

    feat: implement Item database model

--- db/models.py
+++ db/models.py
@@ -7,3 +7,10 @@ class User(Base):
     tg_id = Column(Integer, unique=True, index=True)
     username = Column(String, nullable=True)
     balance = Column(Float, default=0.0)
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Float, nullable=False)
+    description = Column(String, nullable=True)
